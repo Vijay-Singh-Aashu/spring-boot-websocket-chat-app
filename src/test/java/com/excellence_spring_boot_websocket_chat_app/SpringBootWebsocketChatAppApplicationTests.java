a0379ddacc9d0eff7e384691eed056974665a5f6
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-19 11:44:09 +0530

    base-project-created

---
+++ src/test/java/com/excellence_spring_boot_websocket_chat_app/SpringBootWebsocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_websocket_chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
